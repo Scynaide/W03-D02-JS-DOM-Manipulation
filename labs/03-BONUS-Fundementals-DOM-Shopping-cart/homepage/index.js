ddcce435af750718898ebc8a5ccd3f1aa8a7638e
Omar <omarak@gmail.com>
2024-10-15 13:50:37 +0300

    Day 2

---
+++ labs/03-BONUS-Fundementals-DOM-Shopping-cart/homepage/index.js
@@ -0,0 +1,10 @@
+/* 
+TITLE: Shopping cart Lab
+
+1. Add an onclick event to each item in the store that will add the item to the shopping cart
+
+2. When the button is pressed the shopping cart should show the item that was clicked
+
+3. BONUS: Show the total cost in the shopping cart
+
+*/
